6be564d034f63ce5f559728ce39ca7a9924e193d
pashute <pashute@gmail.com>
2017-10-03 14:00:01 +0300

    first time

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template id="main">
+  <v-ons-navigator
+    :page-stack="pageStack"
+    @push-page="pageStack.push($event)">
+  </v-ons-navigator>
+  
+</template>
+<script>
+  export default{
+    data() {
+      return {
+        title: 'Noam v.098.vue'
+      };
+    },
+    methods: {
+      bla() {
+        //this.$ons.notification.alert('Onsen UI here');
+      }
+    }
+  };
+</script>
---
+++ src/tab2.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="homePage">
+    <v-ons-page>
+      <h2>Page1</h2>
+      <v-ons-button @click="changeTab">Change tab via tabbar</v-ons-button>
+      <v-ons-button @click="changeButton">Change tab via segment</v-ons-button>
+      <v-ons-button @click="logIndexes">Log current button index</v-ons-button>
+    </v-ons-page>
+  </div>
+</template>
+<script>
+export default {
+  name: "homePage",
+  data: () => ({
+
+  }),
+  props: ['changeTab', 'changeButton', 'logIndexes']
+}
+</script>
+<style lang="scss" scoped>
+</style>
